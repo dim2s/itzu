28806587841a3bf65b0b9467cf4227a2bf39f2f6
kobs <medi.diop@gmail.com>
2017-04-18 09:11:30 +0200

    csv export using Config variable

--- client/common.js
+++ client/common.js
@@ -1,3 +1,4 @@
+/* global $ Chart document Config str_opModal str_chnModal Channel wizard*/
 /* unordered ToDo list:
 	child row with additional info: unit, description, etc..
 	hidden column unit, description, type, targetsRow
@@ -30,21 +31,21 @@ $(document).ready(function() {
 		"scrollCollapse": true,
 		"select" : { 
 			style: "os", 
-			selector: 'td:first-child'
+			selector: "td:first-child"
 		}
 	});
 
-	var t2 = $('#chnDataTable').DataTable({
+	var t2 = $("#chnDataTable").DataTable({
 		columnDefs:[
-			{className: 'select-checkbox', targets: 0, width: "10px", defaultContent: ""},
+			{className: "select-checkbox", targets: 0, width: "10px", defaultContent: ""},
 			{
 				title:"Label", 
 				targets:1, 
 				className: "chnLabel", 
 				data: "chnLabel", 
-				render: function (d,t,r,m) { 
+				render: function (data ,type ,row ,meta ) { 
 					return "<a href='#' data-toggle='popover' data-placement='bottom' data-container='body' data-content='"
-						+r['chnDesc'] +"'>"+ d + "</a>"  ;
+						+row["chnDesc"] +"'>"+ data + "</a>"  ;
 				}
 			},
 			{
@@ -53,23 +54,23 @@ $(document).ready(function() {
 				className: "chnValue",
 				data: null, //"chnValue",
 				render: function( data, type, row, meta) {
-					var idSet = $('#opDataTable').DataTable().rows('.selected').ids().toArray();
+					var idSet = $("#opDataTable").DataTable().rows(".selected").ids().toArray();
 					var status = row.chnSetValues.status( idSet ) ;
-					var str = '';
+					var str = "";
 
 					switch (status) {
-						case "sticky" :
-							str=row.chnSetValues.default + " " + row["chnUnit"] + "<span class='glyphicon glyphicon-pushpin'></span>";
-							break;
-						case "multi" :
-							str = "<span class='glyphicon glyphicon-option-horizontal'></span>";
-							break;
-						case "empty" :
-							str = "<span class='glyphicon glyphicon-asterisk'></span>";
-							break;
-						case "single" : 
-							str = row.chnSetValues.filter(idSet)[0].value + " " + row["chnUnit"];
-							break;
+					case "sticky" :
+						str=row.chnSetValues.default + " " + row["chnUnit"] + "<span class='glyphicon glyphicon-pushpin'></span>";
+						break;
+					case "multi" :
+						str = "<span class='glyphicon glyphicon-option-horizontal'></span>";
+						break;
+					case "empty" :
+						str = "<span class='glyphicon glyphicon-asterisk'></span>";
+						break;
+					case "single" : 
+						str = row.chnSetValues.filter(idSet)[0].value + " " + row["chnUnit"];
+						break;
 					}
 					return str; 
 				} 
@@ -80,40 +81,40 @@ $(document).ready(function() {
 			{title:"Desc", targets:6, className : "chnDesc", data: "chnDesc" , visible: false}
 
 		],
-		dom: '<"#chnToolbar">Brtip',
+		dom: "<'#chnToolbar'>Brtip",
 		buttons: [ // ** do not change the name attribute **
 			{ 
 				text:"New",
 				name: "new",
 				action: function(event,dt,button, config ) {
-						rowEditFormCreate(button,str_chnModal,dt.table().node().id).modal('show');
-					}
+					rowEditFormCreate(button,str_chnModal,dt.table().node().id).modal("show");
+				}
 			},
 			{ 
 				text:"Remove",
 				name: "remove",
 				action:	function( event, dt, btn, config ) {
-						removeRows(event, dt, btn, config);
-					}
+					removeRows(event, dt, btn, config);
+				}
 			},
 			{ 
 				text:"Edit",
 				name: "edit",
 				state:false,
 				action: function(event,dt,button,config) {
-						editRow(event,dt,button,str_chnModal);
-					}
+					editRow(event,dt,button,str_chnModal);
+				}
 			},
 			{ 
 				text:"Merge All",
 				name: "mergeAll",
 				action: function( event, dt, button, config) {
-					var rows = dt.rows('.selected');
-					
+					var rows = dt.rows(".selected");
+
 					rows.every( function (rowIdx, tableLoop, rowLoop ) {
 						dt.row( rowIdx ).data().chnSetValues.mergeAll(dt.row( rowIdx ).data().chnValue);
 					});
-					dt.rows().invalidate('data').draw('false');
+					dt.rows().invalidate("data").draw("false");
 				}
 			},
 			{ 
@@ -127,22 +128,30 @@ $(document).ready(function() {
 	});
 
 // rowReorder seems not to be compatible with select	
-	var t1 = $('#opDataTable').DataTable({
+	var t1 = $("#opDataTable").DataTable({
 		//ajax : {
 		//	url :"/opImport.json",
 		//	dataSrc: ''
 		//},
 		rowId: "opId",
 		"columnDefs":[
-			{ orderable: false, targets: '_all'},
-			{ title: "<span id='op-select-all'></span>"     ,  targets:0 , data: null,       className: 'select-checkbox',  width: "10px", defaultContent: "" },
+			{ orderable: false, targets: "_all"},
+			{ title: "<span id='op-select-all'></span>"     ,  targets:0 , data: null,       className: "select-checkbox",  width: "10px", defaultContent: "" },
 			{ name: "index",	title: "#",		targets:1 , defaultContent: ""},
-			{ name: "mode",		title: "Mode",		targets:2 , data: "opMode" ,	},
+			{ 
+				name: "mode",
+				title: "Mode",
+				targets:2,
+				data: "opMode" ,
+				render: function ( data, type, row) {
+					return type === "export" ?  Config.csv.mode[data] : data;
+				}
+			},
 			{ name: "dyno",		title: "Dyno",		targets:3 , data: "opDyno" ,	},
 			{ name: "engine",	title: "Engine",	targets:4 , data: "opEngine",	},
 			{ name: "time",		title: "Time",		targets:5 , data: "opTime" ,	},
 			{ name: "active",	title: "Activ",		targets:6 , data: "opActive",	 visible: false , defaultContent: "1" },
-			{ name: "state", 	title: "State",		targets:7 , data: null,		className: 'active-control', defaultContent: "<i></i>" },
+			{ name: "state", 	title: "State",		targets:7 , data: null,		className: "active-control", defaultContent: "<i></i>" },
 			{ name: "selected",	title: "Selected",	targets:8 , data: "opSelected",	visible: false , defaultContent: "0" },
 			{ name: "id",		title: "Id",		targets:9 , data: "opId",	visible: false , width: "10px",  },
 			{ 
@@ -152,39 +161,39 @@ $(document).ready(function() {
 				data: null,
 				visible: false,
 				render: function ( data, type, row) {
-						return type === "export" ?  channelsCsvValue(row.opId.toString()) : data;
-					}
+					return type === "export" ?  channelsCsvValue(row.opId.toString()) : data;
+				}
 			},
 		],
-		dom: '<"#opToolbar">Brtip',
+		dom: "<'#opToolbar'>Brtip",
 		buttons: [
 			{ 
 				text: "New" ,
 				name: "new",
 				action:	function(event,dt,button, config ) {
-						rowEditFormCreate(button,str_opModal,dt.table().node().id).modal("show");
-					}
+					rowEditFormCreate(button,str_opModal,dt.table().node().id).modal("show");
+				}
 			},
 			{
 				text: "Remove",
 				name: "remove",
 				action: function( event, dt, btn, config ) {
-						removeRows(event, dt, btn, config);
-						toggleDataTable_EditRemoveBtn(dt) ;
-					}
+					removeRows(event, dt, btn, config);
+					toggleDataTable_EditRemoveBtn(dt) ;
+				}
 			},
 			{
 				text: "Edit",
 				name: "edit",
 				action: function(event,dt,button,config) {
-						editRow(event,dt,button,str_opModal);
-					}
+					editRow(event,dt,button,str_opModal);
+				}
 			},
 			{ 
 				text: "Wizard" ,
 				action:	function(event, dt, button, config) {
-						wizardFormCreate(button,str_wizardModal,dt.table().node().id).modal("show");
-					}
+					wizardFormCreate(button,str_wizardModal,dt.table().node().id).modal("show");
+				}
 			},
 			{ 
 				text: "Export CSV", 
@@ -194,17 +203,17 @@ $(document).ready(function() {
 					columns: ["activ:name","mode:name","engine:name","dyno:name","time:name","channels:name"],
 					orthogonal: "export",
 					format: {
-						 header : function ( data, columnIdx ) {
-							 if ( data === "Channels" ) {
-								 return channelsCsvHeader();
-							 } else {
+						header : function ( data, columnIdx ) {
+							if ( data === "Channels" ) {
+								return channelsCsvHeader();
+							} else {
 								return Config.csv.header[data]; 
-							 }
-						 }
-					 }
+							}
+						}
+					}
 				},
 				customize: function (csv) {
-					return csv.replace(/\"/g, '');
+					return csv.replace(/\"/g, "");
 				}
 			} ,
 			{ text: "Import CSV" }
@@ -217,7 +226,7 @@ $(document).ready(function() {
 	function channelsCsvValue(opId) {
 		var line = [];
 		$("#chnDataTable").DataTable().rows().data().each( function ( data, index ) {
-			line.push([data.chnSetValues.value(opId),data.chnTrigger].join(Config.csv.valueSeparator));
+			line.push([data.chnSetValues.value(opId),Config.csv.trigger[data.chnTrigger]].join(Config.csv.valueSeparator));
 		});
 
 		return line.join(Config.csv.fieldSeparator);
@@ -226,7 +235,7 @@ $(document).ready(function() {
 	function channelsCsvHeader() {
 		var header = [];
 		$("#chnDataTable").DataTable().rows().data().each( function ( data, index ) {
-			header.push([data.chnLabel, data.chnType].join(Config.csv.valueSeparator));
+			header.push([data.chnLabel, Config.csv.type[data.chnType]].join(Config.csv.valueSeparator));
 		});
 		return header.join(Config.csv.fieldSeparator);
 	}
@@ -242,7 +251,7 @@ $(document).ready(function() {
 	} );
 
 	t1.on( "select deselect", function (e,dt,type,index) {
-		selectHandler( dt, index, e.type ) 
+		selectHandler( dt, index, e.type );
 		// we need to re-render '#chnDataTable' to update the polyvalency attribute
 		t2.rows().invalidate("data").draw("false");
 	} ).draw();
@@ -261,31 +270,31 @@ $(document).ready(function() {
 	//************************************* jQuery event handlers **********************************************
 	//Todo: bugfix when using global select the chart is not refresh
 	$("#opDataTable_wrapper").on( "click" ,"th.select-checkbox",  function () {
-			var dt=$("#opDataTable").DataTable();
+		var dt=$("#opDataTable").DataTable();
 
-			// toggle select status for all rows	
-			if (dt.rows(".selected").count() > 0) {
-				dt.rows().deselect().draw();
-			} else { 
-				dt.rows().select().draw();
-			}
+		// toggle select status for all rows	
+		if (dt.rows(".selected").count() > 0) {
+			dt.rows().deselect().draw();
+		} else { 
+			dt.rows().select().draw();
+		}
 	});
 
 	// toggle Activ cell value on click
 	$("#opDataTable tbody").on( "click", "td.active-control" , function () {
-			var tr = $(this).closest("tr");
-			var tr_idx = tr.index();
-			var d = t1.row(tr_idx).data();
+		var tr = $(this).closest("tr");
+		var tr_idx = tr.index();
+		var d = t1.row(tr_idx).data();
 
-			if ( d["opActive"] === 0 ){
-				d["opActive"] = 1;
-				tr.removeClass( "ban");
-			}else{
-				d["opActive"] = 0;
-				tr.addClass( "ban");
-			}
+		if ( d["opActive"] === 0 ){
+			d["opActive"] = 1;
+			tr.removeClass( "ban");
+		}else{
+			d["opActive"] = 0;
+			tr.addClass( "ban");
+		}
 
-			t1.row(tr_idx).data(d).draw();
+		t1.row(tr_idx).data(d).draw();
 	});
 
 	/*TODO: triggered when a new label is selected
@@ -305,7 +314,6 @@ $(document).ready(function() {
 	$(document).on("submit", "#form-wizard", function(e) {
 		var rows = [];
 		var data = [];
-		var m=$("#form-wizard");
 
 		var f = $("#form-wizard").serializeArray().reduce(function(obj,item) {
 			obj[item.name] = item.value;
@@ -342,7 +350,7 @@ $(document).ready(function() {
 		var dt = $(table_id).DataTable();
 		var inputObj = {};
 
-		$("#formModal *").filter(".dataTableAutoImport").each( function(i) {
+		$("#formModal *").filter(".dataTableAutoImport").each( function() {
 			inputObj[$(this).attr("id")]= $(this).val() ;
 		});
 		
@@ -354,7 +362,7 @@ $(document).ready(function() {
 		// in any cases during saving operation of 'chnDataTable' form
 		// the saved value is applied to the selection of rows in '#opDataTable'
 		var set = {};
-		if (inputObj.source === '#chnDataTable') {
+		if (inputObj.source === "#chnDataTable") {
 			set.value = inputObj.chnValue;
 			set.targets = new Set ($("#opDataTable").DataTable().rows(".selected").ids().toArray());
 		}
@@ -375,7 +383,7 @@ $(document).ready(function() {
 			}
 			dt.row.add(inputObj).draw();
 		} else { 
-			//we can use indexes as we're supposed to have only on row selected
+			//we can use indexes as we're supposed to have only one row selected
 			//make sure it is always the case other wise it is a bug
 			var idx=dt.rows(".selected").indexes();
 			if ( inputObj.source === "#opDataTable" ) {
@@ -386,7 +394,7 @@ $(document).ready(function() {
 			
 			// no intersection allowed between the current selection and
 			// all other chnTargets
-			if ( inputObj.source = "#chnDataTable" ) {
+			if ( inputObj.source === "#chnDataTable" ) {
 				inputObj.chnSetValues = dt.row(idx).data().chnSetValues;
 				inputObj.chnSetValues.add(set.value, set.targets);
 			}
@@ -397,13 +405,13 @@ $(document).ready(function() {
 
 	//************************************* datatable common logic **********************************************
 	if (t1.rows().count() === 0)
-		t1.buttons(['remove:name','edit:name']).disable();
+		t1.buttons(["remove:name","edit:name"]).disable();
 
-	t2.buttons(['new:name','edit:name','remove:name','mergeAll:name']).disable();
+	t2.buttons(["new:name","edit:name","remove:name","mergeAll:name"]).disable();
 
 	// toolbar definition
-	$('#opToolbar').html('<h4>Operating points definition</h4>');
-	$('#chnToolbar').html('<h4>Channels definition</h4>');
+	$("#opToolbar").html("<h4>Operating points definition</h4>");
+	$("#chnToolbar").html("<h4>Channels definition</h4>");
 
 	//************************************* helper function **********************************************
 	// make sure that chnDataTable 'new' button is activated only if 
@@ -436,28 +444,23 @@ $(document).ready(function() {
 	// * you can remove several rows at the same time
 	// * items should be selected to be edit or remove	
 	function toggleDataTable_EditRemoveBtn( dt ) { 
-		var count = dt.rows('.selected').count();  
+		var count = dt.rows(".selected").count();  
 		if ( count === 0 )
-			dt.buttons(['remove:name','edit:name']).disable();
+			dt.buttons(["remove:name","edit:name"]).disable();
 		if ( count === 1 )
-			dt.buttons(['remove:name','edit:name']).enable();
+			dt.buttons(["remove:name","edit:name"]).enable();
 		if (count > 1) {
-			dt.button('edit:name').disable();
-			dt.button('remove:name').enable();
+			dt.button("edit:name").disable();
+			dt.button("remove:name").enable();
 		}
 	}
 
 	function selectRowToDraw(mode) {
-		return $('#opDataTable')
+		return $("#opDataTable")
 			.DataTable()
 			.rows( function(idx,d,node) { return (d.opMode == mode) ? true : false ;} )
 			.data()
 			.toArray();
-	}
-
-	function toggleNodeSelectActive(idx) {
-		var isActive = ( $('circle#' + idx).attr('data-active') == "true");
-		$('circle#' + idx).attr('data-active', !isActive );
 	}
 
 	function selectHandler( dt, index, action ) {
@@ -470,7 +473,7 @@ $(document).ready(function() {
 			dt.cell(i,8).data( s ) ;
 		});
 
-		var selectedRows = dt.rows('.selected');
+		var selectedRows = dt.rows(".selected");
 		if (selectedRows.count() > 0 ) {
 			targetMode = selectedRows.data()[0].opMode;
 			var data = selectRowToDraw(targetMode); 
@@ -480,69 +483,53 @@ $(document).ready(function() {
 	}
 
 	function removeRows(event, dt, btn, config) {
-		dt.rows('.selected').remove().draw();
+		dt.rows(".selected").remove().draw();
 		if ( dt.rows().count() === 0)
-			dt.buttons(['remove:name','edit:name']).disable();
-	};
+			dt.buttons(["remove:name","edit:name"]).disable();
+	}
 
 	//type = 'chn' or 'op' 
 	//mais est-ce vraiment necessaire puisqu'il s'agit de la meme fenetre
 	function rowEditFormCreate(b,str,dt_id) {
-		var m=$('#formModal');
-		m.find('.modal-footer').append(
+		var m=$("#formModal");
+		m.find(".modal-footer").append(
 			'<button type="button" class="btn btn-primary contextualItems" id="ListModify" data-tableid="' +dt_id+ '" data-btntype="'+b.text()+'">Save</button>'
 		//	'<input type="submit" class="btn btn-primary contextualItems" id="btn-newSetvalue" value="Save" form="formNewSetValue">'
 		);
-		m.find('.modal-title').text(b.text());
-		m.find('.modal-body').append(str);
+		m.find(".modal-title").text(b.text());
+		m.find(".modal-body").append(str);
 
 		//initialize the datalist only for chnDataTable
-		if ( dt_id === 'chnDataTable' )
+		if ( dt_id === "chnDataTable" )
 			loadChnList();
 
 		return m;
 	}
 
 	function wizardFormCreate(b,str,dt_id) {
-		var m = $('#formModal');
+		var m = $("#formModal");
 
-		m.find('.contextualItems').remove();
-		m.find('.modal-title').text(b.text());
-		m.find('.modal-body').append(str);
-		m.find('.modal-footer').append(
-			'<input type="submit" class="btn btn-primary contextualItems" id="btn-wizard" value="Save" form="form-wizard">'
+		m.find(".contextualItems").remove();
+		m.find(".modal-title").text(b.text());
+		m.find(".modal-body").append(str);
+		m.find(".modal-footer").append(
+			"<input type='submit' class='btn btn-primary contextualItems' id='btn-wizard' value='Save' form='form-wizard'>"
 		);
 		return m;
 	}
 
 	function editRow(e,dt,button,template) {
-		var rowSelected = dt.rows( '.selected' );
+		var rowSelected = dt.rows( ".selected" );
 
 		if( rowSelected.count() === 1) {
 			var m= rowEditFormCreate(button,template,dt.table().node().id ); 
-			console.log(rowSelected.data()[0]);	
-			$('#formModal *').filter('.dataTableAutoImport').each( function(i) {
-				$(this).val(rowSelected.data()[0][$(this).attr('id')]) ;
+			$("#formModal *").filter(".dataTableAutoImport").each( function() {
+				$(this).val(rowSelected.data()[0][$(this).attr("id")]) ;
 			});
 
-			m.modal('show'); 
+			m.modal("show"); 
 		} else {
-			alert( 'something goes wrong! you should not be in this state....' );
+			alert( "something goes wrong! you should not be in this state...." );
 		}
-	}
-
-	// return an array containing all the values for a given channels and a selection of operating
-	// points	
-	// Polyvalency:
-	// 	* single: a unique value for the selection ( only value )
-	// 	* multiple: several value for the selection ( glyphicon option-horizontal)
-	// 	* empty: no value for the selection (glyphicon: asterisk)
-	// 	* sticky: same value for all, even for ones not yet created (value + glyphicon: pushpin)
-	function getChnValuesFromSelection( idSet , channel ) {
-		return channel.chnTargets.filter ( function(obj) {
-			return (obj.targets.intersection(idSet).size > 0);
-		}).map ( function(obj) {
-			return obj.value;
-		});
 	}
 }); // function.ready({})
--- client/config.js
+++ client/config.js
@@ -1,11 +1,32 @@
 var Config = {
 	csv : {
 		header : {
-			"Engine"	:"T_Engine",
-			"Dyno"		:"T_Dyno",
-			"Mode"		:"T_Mode",
-			"Activ"		:"Activ",
-			"Time"		:"CtrlTime"
+			"Engine"	:"TST_ENGINE_TARGET",
+			"Dyno"		:"TST_DYNO_TARGET",
+			"Mode"		:"TST_CMOD",
+			"Activ"		:"TST_ACTIV_OP",
+			"Time"		:"TST_SSQ_LENGHT"
+		},
+		trigger : {
+			"Before": 0,
+			"After" : 1
+		},
+		type : {
+			"Map"	: "map_ecu",
+			"Curve"	: "curve_ecu",
+			"Value"	: "param_ecu",
+			"NN"	: "param",
+			"Draft"	: "param" 
+		},
+		mode : {
+			"IDLE"		:	9,
+			"IDLE_CONTROL"	:	10,
+			"N/ACCEL"	:	8,
+			"N/C_BRT_5H"	:	12,
+			"N/X_VALUE"	:	16,
+			"C_BRT_5H/ACCEL":	11,
+			"C_BRT_5H/N"	:	13,
+			"C_BRT_5H/X_VALUE":	17
 		},
 		fieldSeparator : ";",
 		valueSeparator : "/",

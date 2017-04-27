bcce62b3b59eb020dfff75d4f8767a6c30eedbb5
kobs <medi.diop@gmail.com>
2017-04-27 13:17:50 +0200

    this kind of 'templates' is no more needed

--- client/fileUploadModal.js
+++
@@ -1,8 +0,0 @@
-/* Note:
- * 1 - element of dataTableAutoImport class must have an id which match exactly the data columns field data field 
- */
-var str_fileUploadModal = `
-<label class="btn btn-primary contextualItems" for="file-selector">
-	<input id="file-selector" type="file" style="display:none;" >Select file
-</label>
-`;
